c15df6de179a97735078704a193d9d16b5e34a34
hectopk <hectopk@gmail.com>
2018-08-27 14:28:51 +0300

    bug fix

--- argentit/urls.py
+++ argentit/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls import url
+from django.views.generic import RedirectView
 
 urlpatterns = [
 	url(r'^select2/', include('django_select2.urls')),
 	url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 	path('', include('alfa.urls')),
-	#path('djadmin', admin.site.urls),
+	url(r'', RedirectView.as_view(pattern_name='home_url', permanent=False))
 ]
 
 #urlpatterns += staticfiles_urlpatterns()
