6adf8ac6a8a440857be6c0263de50c62f90b68f0
hectopk <hectopk@gmail.com>
2018-09-09 23:14:52 +0300

    add robots.txt

--- alfa/views/views.py
+++ alfa/views/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.conf import settings
 
 def robots_txt(request):
-	content = {}
-	return HttpResponse(content_type='text/plain')
+	try:
+		path = settings.BASE_DIR + '/../robots.txt'
+		file = open(path, 'r')
+		content = file.read()
+		return HttpResponse(content, content_type='text/plain')
+	except Exception as e:
+		print(e)
+		return HttpResponse(content_type='text/plain')
