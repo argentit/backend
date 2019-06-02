0f68b2c927ada065e5146d5bae2b6a7a5cf1eb82
hectopk <hectopk@gmail.com>
2019-06-02 22:42:57 +0300

    add index

--- argentit/settings.py
+++ argentit/settings.py
@@ -106,9 +106,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))+'/'
+# PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = PROJECT_PATH
+# STATIC_ROOT = os.path.join(PROJECT_PATH, 'static/')
 
 STATIC_URL = '/static/'
 
